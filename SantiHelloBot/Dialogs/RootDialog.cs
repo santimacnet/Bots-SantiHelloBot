54256f6cfcbdeb9c63735f2d48e3551783d652ce
smacias.rodriguez <smacias.rodriguez@AT-4FZRFH2>
2018-01-03 16:50:32 +0100

    Agregar logica del bot en "Root Dialog"

--- SantiHelloBot/Dialogs/RootDialog.cs
+++ SantiHelloBot/Dialogs/RootDialog.cs
@@ -18,12 +18,45 @@ namespace SantiHelloBot.Dialogs
         private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
         {
             var activity = await result as Activity;
-
-            // calculate something for us to return
             int length = (activity.Text ?? string.Empty).Length;
 
-            // return our reply to the user
-            await context.PostAsync($"You sent {activity.Text} which was {length} characters");
+            // return our reply to the user 
+            if (activity.Text.Contains("hola"))
+            {
+                await context.PostAsync("Hola Qtal!!");
+            }
+            else if (activity.Text.Contains("adios"))
+            {
+                await context.PostAsync("Adios, hasta pronto...");
+            }
+            else if (activity.Text.Contains("llamas"))
+            {
+                await context.PostAsync("Me llamo SantiBot");
+            }
+            else if (activity.Text.Contains("quien eres"))
+            {
+                await context.PostAsync("Soy un ChatBot de pruebas");
+            }
+            else if (activity.Text.Contains("dias"))
+            {
+                await context.PostAsync("Buenos dias, hora de trabajar!!");
+            }
+            else if (activity.Text.Contains("noches"))
+            {
+                await context.PostAsync("Buenas noches");
+            }
+            else if (activity.Text.Contains("fecha"))
+            {
+                await context.PostAsync($"Hoy es {DateTime.Now.ToShortDateString()}");
+            }
+            else if (activity.Text.Contains("hora"))
+            {
+                await context.PostAsync($"Son las {DateTime.Now.ToShortTimeString()}");
+            }
+            else
+            {
+                await context.PostAsync($"You sent {activity.Text} which was {length} chars");
+            }
 
             context.Wait(MessageReceivedAsync);
         }
